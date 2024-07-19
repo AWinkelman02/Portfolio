9690a13379d745f3fa58e0e0f2b9a06ba8fca294
AWinkelman02 <awinkelman02@gmail.com>
2024-07-19 16:24:24 -0500

    Adding favicon and body background animations.

--- particle-background.js
+++ particle-background.js
@@ -1,91 +1,65 @@
-const PARTICLE_COUNT = 30 * 2;
-const Particles = [];
+var canvas = document.getElementById('body-canvas');
 
-function rand(min, max) {
-  return Math.random() * (max - min) + min;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+var c = canvas.getContext('2d');
+
+function Circle(x, y, dx, dy, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    
+    c.fillStyle = `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`;
+    c.fill();
+  }
+  
+  this.update = function() {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0){
+    this.dx = -this.dx;
+    }
+
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0){
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+    
+    this.draw();
+   }
 }
 
-function normalPool(o){var r=0;do{var a=Math.round(normal({mean:o.mean,dev:o.dev}));if(a<o.pool.length&&a>=0)return o.pool[a];r++}while(r<100)}function randomNormal(o){if(o=Object.assign({mean:0,dev:1,pool:[]},o),Array.isArray(o.pool)&&o.pool.length>0)return normalPool(o);var r,a,n,e,l=o.mean,t=o.dev;do{r=(a=2*Math.random()-1)*a+(n=2*Math.random()-1)*n}while(r>=1);return e=a*Math.sqrt(-2*Math.log(r)/r),t*e+l}
+var circleArray = [];
 
-
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-let WIDTH = document.body.clientWidth;
-let HEIGHT = document.body.clientHeight;
-const DEPTH = 5;
-const SPEED1 = 0.001;
-
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
-
-window.onresize = function() {
-  WIDTH = document.body.clientWidth;
-  HEIGHT = document.body.clientHeight;
-  canvas.width = WIDTH;
-  canvas.height = HEIGHT;
-}
-
-function spawnNew(start = false) {
-  const p = {
-    x: Math.random(),
-    y: start ? 1.1 : rand(0, 1.5), // spawn in slightly off-screen, to avoid jarring pop-in
-    z: Math.random(),
+for (var i = 0; i < 150; i++){
+  var radius = Math.random() * 3;
+  var x = Math.random() * (innerWidth - radius * 2) + radius;
+  var y = Math.random() * (innerHeight - radius * 2) + radius;
+  var dx = (Math.random() - 0.5);
+  var dy = (Math.random() - 0.5);
+  //Random color
+  var color = {
     r: 0,
-    g: randomNormal({ mean: 125, dev: 30 }),
-    b: randomNormal({ mean: 125, dev: 30 }),
-    a: 1
+    g: randomNormal({ mean: 125, dev: 20 }),
+    b: randomNormal({ mean: 125, dev: 20 }),
+    a: rand(0, 1),
   };
-  p.startingX = p.x;
-  p.startingY = p.y;
-  Particles.push(p)
+  circleArray.push(new Circle(x, y, dx, dy, radius, color))
 }
 
-if (Particles.length < PARTICLE_COUNT) {
-    for (var l = 0; l < 50; l++) {
-      spawnNew(true);
-    }
-  }
-
-function render() {
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  
-  ctx.fillStyle = "white";
-  
-  const delet = [];
-  
-  for (let i = 0; i < Particles.length; i++) {
-    let {x, y, z, r, g, b, a} = Particles[i];
-    const sideLen = Math.floor(z * DEPTH);
-    
-    const screenX = Math.floor(((WIDTH * x) - (sideLen / 2)) * 10) / 10;
-    const screenY = Math.floor(((HEIGHT * y) - (sideLen / 2)) * 10) / 10;
-    
-    //const color = 100 //z * 255;
-
-    ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`;
-    ctx.fillRect(screenX, screenY, sideLen, sideLen);
-    
-    y -= SPEED1 * z;
-    if (y < 0) {
-      delet.push(i);
-      // delete Particles[i];
-      spawnNew(true);
-    } else {
-      Particles[i].y = y;
-    }
-  }
-  
-  for (let i = 0; i < delet.length; i++) {
-    Particles.splice(delet[i] - i, 1);
-  }
-  
-  if (Particles.length < PARTICLE_COUNT) {
-    for (var l = 0; l < 50; l++) {
-      spawnNew(true);
-    }
-  }
-  
-  window.requestAnimationFrame(render);
+function animate (){
+  requestAnimationFrame(animate)
+  c.clearRect(0, 0, innerWidth, canvas.height);
+  circleArray.forEach(function(elem) {
+    elem.update();
+  })
 }
-window.requestAnimationFrame(render);
+
+animate();
